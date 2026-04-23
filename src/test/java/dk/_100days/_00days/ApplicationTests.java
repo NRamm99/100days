c6f27e3aa2c2ac18968a91027e16dc288eb14e1d
Nichlas Ramm <162293775+NRamm99@users.noreply.github.com>
2026-04-23 17:34:38 +0200

    Initial commit

---
+++ src/test/java/dk/_100days/_00days/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dk._100days._00days;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
